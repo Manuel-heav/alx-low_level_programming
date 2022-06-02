acaa4ceedc0ca0120bd1bc87125a1bf26cbc65d0
manuel-heav <manuelheav32@gmail.com>
2022-06-02 20:42:30 +0300

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: always 0 (success)\
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
