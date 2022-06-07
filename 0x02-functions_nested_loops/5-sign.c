283a77d1f2dee050f3f860304ee83c27dc389255
manuel-heav <manuelheav32@gmail.com>
2022-06-07 19:55:00 +0300

    functions and loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,17 @@ int print_sign(int c)
 {
 if (c > 0)
 {
-_putchar('+')
+_putchar('+');
 return (1);
 }
 else if (c < 0)
 {
-_putchar('-')
+_putchar('-');
 return (-1);
 }
 else
-_putchar('0')
+{
+_putchar('0');
 return (0);
+}
 }
