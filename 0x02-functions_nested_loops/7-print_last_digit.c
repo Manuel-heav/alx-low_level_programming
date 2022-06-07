5a382f5633ca1cfec09016e24a5a608055d0ab56
manuel-heav <manuelheav32@gmail.com>
2022-06-07 20:22:59 +0300

    functions and loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * print_last_digit - the function that gets us the last digit
+ * @c: the number to restract the last digit from
+ *
+ * Return: always 0.
+ */
+int print_last_digit(int c)
+{
+return (c % 10);
+}
