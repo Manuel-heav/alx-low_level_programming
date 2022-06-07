4001b3757266603a47755ee0f462ceabe89f1a01
manuel-heav <manuelheav32@gmail.com>
2022-06-07 20:05:37 +0300

    functions and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - finds the absolute value
+ * @c: the number to be converted
+ *
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+int a = abs(c);
+printf("%d\n", a);
+}
