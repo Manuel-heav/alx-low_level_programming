398bdb7330db8a41a4b412dd3aebb90c7ec5f71b
manuel-heav <manuelheav32@gmail.com>
2022-06-07 19:28:43 +0300

    functions and loops

---
+++ 0x02-functions_nested_loops/2-print_alphapet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i = 0;
+for (i = 0; i <= 10; i++)
+{
+_putchar('abcdefghijklmnopqrstuvwxyz');
+}
+_putchar('\n');
+}
