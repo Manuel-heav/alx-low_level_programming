0811fdcb338a978a7bc22431af12ad2df083c9b2
manuel-heav <manuelheav32@gmail.com>
2022-06-07 20:48:49 +0300

    functions and loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,21 +4,26 @@
  */
 void times_table(void)
 {
-int n, mult, prod;
-for (n = 0; n <= 9; n++)
-{
-_putchar('0');
-for (mult = 1; mult <= 0; mult++)
-{
-_putchar(',');
-_putchar(' ');
-prod = n * mult;
-if (prod <= 9)
-_putchar(' ');
-else
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-_putchar('\n');
-}
+	int n, mult, prod;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = n * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
