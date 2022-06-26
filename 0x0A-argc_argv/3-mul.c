ca2179691d4aecfb109ed96c840a983f2df487d3
manuel-heav <manuelheav32@gmail.com>
2022-06-26 09:55:05 +0300

    new commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: If the program receives two arguments - 0.
+ * Return: If the program receives two arguments - 0
  *         If the program does not receive two arguments - 1
  */
 int main(int argc, char *argv[])
