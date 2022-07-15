b8a31ca76e4556b6df5a0ceef7fd6108ae8125bf
manuel-heav <manuelheav32@gmail.com>
2022-07-15 19:37:57 +0300

    new commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
+ * @argc: The number of arguments supplied to the program
  * @argv: An array of pointers to the arguments.
  *
  * Return: Always o.
